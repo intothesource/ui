d4f065033458503f71da4dc669d19a02a575942f
Ramon Valk <rvalk@intothesource.com>
2020-02-14 09:36:11 +0100

    Finished toggle v1

--- projects/ui/src/lib/toggle/toggle.component.ts
+++ projects/ui/src/lib/toggle/toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'its-toggle',
@@ -10,10 +10,33 @@ export class ToggleComponent implements OnInit {
   @ViewChild('input', null) input: ElementRef;
   @ViewChild('label', null) label: ElementRef;
 
+  @Input() toggleName: string;
+  @Input() toggleState: string;
+
   constructor() { }
 
   ngOnInit() {
-    console.log(this.input, this.label);
+    this.setRandomName(this.toggleName);
+    this.linkElements(this.toggleName);
+    this.setState(this.toggleState);
+  }
+
+  setRandomName(name) {
+    if (!name) {
+      this.toggleName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
+  }
+
+  linkElements(name) {
+    this.input.nativeElement.setAttribute('name', name);
+    this.input.nativeElement.setAttribute('id', name);
+    this.label.nativeElement.setAttribute('for', name);
+  }
+
+  setState(bool) {
+    if (typeof this.toggleState === "boolean") {
+      this.input.nativeElement.checked = bool;
+    }
   }
 
 }
