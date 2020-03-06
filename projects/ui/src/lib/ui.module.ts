f9517ea19091ba08417743d8d5c3c3614dec5b23
Felix Dulfer <fdulfer@intothesource.com>
2020-03-06 15:03:58 +0100

    WIP

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
-import { UiComponent } from './ui.component';
 import { ButtonComponent } from './button/button.component';
 import { ToggleComponent } from './toggle/toggle.component';
-import { ToggleXComponent } from './toggle-x/toggle-x.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    UiComponent,
     ButtonComponent,
     ToggleComponent,
-    ToggleXComponent,
   ],
   imports: [
     FormsModule
   ],
   exports: [
-    UiComponent,
+    FormsModule,
     ButtonComponent,
     ToggleComponent,
-    ToggleXComponent,
-    FormsModule,
   ]
 })
 export class UiModule { }
