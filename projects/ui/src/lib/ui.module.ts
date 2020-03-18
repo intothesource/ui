9e50c4fc93fc490b22f70f0041a3d4b2fed0958a
Felix Dulfer <fdulfer@intothesource.com>
2020-03-18 16:20:02 +0100

    Offer dropdown as module for storybook

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from './button/button.component';
 import { ToggleComponent } from './toggle/toggle.component';
-import { FormsModule } from '@angular/forms';
-import { DropdownComponent } from './dropdown/dropdown.component';
+import { DropdownModule } from './dropdown/dropdown.module';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     ToggleComponent,
-    DropdownComponent,
   ],
   imports: [
-    FormsModule
+    FormsModule,
+    DropdownModule,
   ],
   exports: [
     FormsModule,
+    DropdownModule,
     ButtonComponent,
     ToggleComponent,
   ]
