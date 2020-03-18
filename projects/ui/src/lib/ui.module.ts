c33eb6b723278b6429f13bfaa449fe9813d04fc1
Ramon Valk <rvalk@intothesource.com>
2020-03-18 16:23:34 +0100

    Added initial ripple-animation files

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -3,12 +3,14 @@ import { ButtonComponent } from './button/button.component';
 import { ToggleComponent } from './toggle/toggle.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { RippleAnimationComponent } from './ripple-animation/ripple-animation.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     ToggleComponent,
     DropdownComponent,
+    RippleAnimationComponent,
   ],
   imports: [
     FormsModule
