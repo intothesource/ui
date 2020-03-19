2f4403f8e52f282064e61b2875275320673775d9
Felix Dulfer <fdulfer@intothesource.com>
2020-03-19 11:42:13 +0100

    Add spec for toggle module

---
+++ projects/ui/src/lib/toggle/toggle.module.spec.ts
@@ -0,0 +1,56 @@
+import { async, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ToggleModule } from './toggle.module';
+
+fdescribe('ToggleModule', () => {
+
+  describe('basic operations', () => {
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ToggleTestComponent],
+        imports: [ToggleModule],
+      })
+
+      TestBed.compileComponents();
+    }));
+
+    it('should create', () => {
+      const fixture = TestBed.createComponent(ToggleTestComponent);
+      const testDebugElement = fixture.debugElement;
+      const toggleDebugElement = testDebugElement.query(By.css('its-toggle'));
+      fixture.detectChanges();
+
+      expect(toggleDebugElement).toBeTruthy();
+    });
+
+    it('should have an input', () => {
+      const fixture = TestBed.createComponent(ToggleTestComponent);
+      const testDebugElement = fixture.debugElement;
+      const toggleDebugElement = testDebugElement.query(By.css('its-toggle'));
+      const inputDebugElement = toggleDebugElement.query(By.css('input'));
+      fixture.detectChanges();
+
+      expect(inputDebugElement).toBeTruthy();
+    });
+
+    it('should have label', () => {
+      const fixture = TestBed.createComponent(ToggleTestComponent);
+      const testDebugElement = fixture.debugElement;
+      const toggleDebugElement = testDebugElement.query(By.css('its-toggle'));
+      const labelDebugElement = toggleDebugElement.query(By.css('label'));
+      fixture.detectChanges();
+
+      expect(labelDebugElement).toBeTruthy();
+    });
+
+  });
+});
+
+@Component({
+  template: `<its-toggle></its-toggle>`
+})
+class ToggleTestComponent { }
