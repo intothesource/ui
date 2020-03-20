cfcb3fe9d0f76a5bf177345ebde2e648d20aec1c
Ramon Valk <rvalk@intothesource.com>
2020-03-20 15:28:44 +0100

    Cleaned up ripple animation

--- projects/ui/src/lib/ripple-animation/ripple-animation.component.ts
+++ projects/ui/src/lib/ripple-animation/ripple-animation.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'its-ripple-animation',
+  selector: '[its-ripple-animation], its-ripple-animation',
   templateUrl: './ripple-animation.component.html',
   styleUrls: ['./ripple-animation.component.css']
 })
 export class RippleAnimationComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
   }
 
 }
