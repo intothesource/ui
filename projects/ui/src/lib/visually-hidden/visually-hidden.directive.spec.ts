0b1cad7fe1fb55ad179a123793a5db2dd82662cb
Felix Dulfer <fdulfer@intothesource.com>
2020-03-21 17:55:05 +0100

    Remove unused dependencies

--- projects/ui/src/lib/visually-hidden/visually-hidden.directive.spec.ts
+++ projects/ui/src/lib/visually-hidden/visually-hidden.directive.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 
