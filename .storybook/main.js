b56ee32e1ae39bc36466717a34b75d81b7338e21
Felix Dulfer <fdulfer@intothesource.com>
2020-03-22 12:35:44 +0100

    Disable Storybook Webpack Performance hints

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
 module.exports = {
   stories: ['../projects/**/*.stories.ts'],
+  webpackFinal: async (config, { configType }) => {
+
+    config.performance = config.performance || {}
+    config.performance.hints = false
+
+    // Return the altered config
+    return config;
+  },
 };
