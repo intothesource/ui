4efcd6b58a47cbd1d1133852f4e4b6f1d4b8d6ba
Ramon Valk <rvalk@intothesource.com>
2020-03-25 11:10:44 +0100

    Add ripple variables

--- projects/ui/src/lib/ripple-animation/ripple-animation.component.ts
+++ projects/ui/src/lib/ripple-animation/ripple-animation.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 
+export const maxRipples = 6;
+
 @Component({
   selector: '[its-ripple-animation], its-ripple-animation',
   templateUrl: './ripple-animation.component.html',
   styleUrls: ['./ripple-animation.component.css']
 })
 export class RippleAnimationComponent implements OnInit {
+
+  rippleAmount: number = 0;
+  rippleAllowed: boolean = true;
 
   @HostListener('click', ['$event'])
   handleClick(event: MouseEvent) {
