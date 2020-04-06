863cf5606e0a2d290aa4cbd2688e964fe29e0cb9
Ramon Valk <rvalk@intothesource.com>
2020-04-06 16:04:36 +0200

    Added scale function

--- projects/ui/src/lib/ripple/ripple.component.ts
+++ projects/ui/src/lib/ripple/ripple.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'its-ripple',
@@ -11,14 +11,19 @@ export class RippleComponent implements OnInit {
   @Input() y: number;
   @Input() color: string;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     console.log(this.x, this.y, this.color);
+    this.setScale(100);
   }
 
   ngOnDestroy(): void {
 
+  }
+
+  setScale(scale: number) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'transform', `scale(${scale})`);
   }
 
 }
